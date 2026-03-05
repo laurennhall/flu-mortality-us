0684cbdc2829174c8c0a39a76042da6a0979b616
laurennhall <lnhall.contact@gmail.com>
2026-03-04 22:51:28 -0500

    Add analysis script for flu deaths data visualization
    This script reads flu death data from a CSV file, processes it to group deaths by year, and generates a line plot of the yearly deaths.

--- analysis.py
+++ analysis.py
@@ -1,1 +1,32 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#reads the csv file
+df = pd.read_csv("data/flu_deaths.csv")
+
+#looks at first few rows
+print(df.head())
+
+#looks at column headers
+print(df.columns())
+
+# group deaths by year
+yearly_deaths = df.groupby("Year")["Deaths"].sum()
+
+print(yearly_deaths)
+
+yearly_deaths.plot(kind="line", marker="o")
+
+plt.title("US Influenza Deaths by Year")
+plt.xlabel("Year")
+plt.ylabel("Deaths")
+plt.tight_layout()
+
+plt.savefig("plots/deaths_by_year.png")
+
+plt.show()
+
+
+
+
 
