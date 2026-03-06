ed730824c9d6a115a36517048eb4f85bc80e9e81
laurennhall <lnhall.contact@gmail.com>
2026-03-05 23:14:27 -0500

    Enhance analysis with flu mortality data visualization
    Added data processing and visualization for flu mortality rates.

--- analysis.R
+++ analysis.R
@@ -1,3 +1,29 @@
-install.packages("ggplot2")
 
+library(dplyr)
 library(ggplot2)
+
+flu_deaths <- read.csv("data/flu-deaths.csv")
+
+head(flu_deaths)
+names(flu_deaths)
+
+ggplot(data = flu_deaths, aes(x = State, y = Crude.Rate)) +
+  geom_col()
+
+highest_cmr <- flu_deaths %>%
+  arrange(desc(Crude.Rate)) %>%
+  head(5)
+
+lowest_cmr <- flu_deaths %>% arrange(Crude.Rate) %>% head(5)
+
+highestandlowest <- bind_rows(highest_cmr, lowest_cmr)
+
+ggplot(data = highestandlowest, aes(x = reorder(State, Crude.Rate), y = Crude.Rate)) +
+  geom_col(fill= "steelblue") +
+  coord_flip() +
+  labs(
+    title = "Highest and Lowest Influenza Mortality Rates by State (2018 - 2023)",
+    x = "State",
+    y = "Crude Mortality Rate"
+  )  +
+  theme_minimal()
